04711c2cc7d7c7cdeea086fb8877a3b34cd6ed20
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-12-28 21:32:50 +0200

    Update Serializer.cs

--- EF Core Exam Preparation 06.08.2022/Footballers/DataProcessor/Serializer.cs
+++ EF Core Exam Preparation 06.08.2022/Footballers/DataProcessor/Serializer.cs
@@ -12,19 +12,25 @@
     {
         public static string ExportCoachesWithTheirFootballers(FootballersContext context)
         {
+            //using Data Transfer Object Class to map it with coaches
             XmlSerializer serializer = new XmlSerializer(typeof(ExportCoachesDTO2[]), new XmlRootAttribute("Coaches"));
 
+            //using StringBuilder to gather all info in one string
             StringBuilder sb = new StringBuilder();
 
+            //"using" automatically closes opened connections
             using var writer = new StringWriter(sb);
 
             var xns = new XmlSerializerNamespaces();
+
+            //one way to display empty namespace in resulted file
             xns.Add(string.Empty, string.Empty);
 
             var coachesAndFootballers = context.Coaches
                 .Where(c => c.Footballers.Any())
                 .Select(c => new ExportCoachesDTO2
                 {
+                    //using identical properties in order to map successfully
                     FootballersCount = c.Footballers.Count,
                     CoachName = c.Name,
                     Footballers = c.Footballers
@@ -40,14 +46,20 @@
                 .ThenBy(c => c.CoachName)
                 .ToArray();
 
+            //Serialize method needs file, TextReader object and namespace to convert/map
             serializer.Serialize(writer, coachesAndFootballers, xns);
+
+            //explicitly closing connection in terms of reaching edge cases
             writer.Close();
 
-            return sb.ToString();
+            //using TrimEnd() to get rid of white spaces
+            return sb.ToString().TrimEnd();
         }
 
         public static string ExportTeamsWithMostFootballers(FootballersContext context, DateTime date)
         {
+            //turning needed info about teams into a collection using anonymous object
+            //using less data
             var teamsAndFootballers = context.Teams
                 .Where(t => t.TeamsFootballers.Any(f => f.Footballer.ContractStartDate >= date))
                 .Select(t => new
@@ -60,7 +72,7 @@
                           .Select(tf => new
                            {
                               FootballerName = tf.Footballer.Name,
-                              ContractStartDate = tf.Footballer.ContractStartDate.ToString("d", CultureInfo.InvariantCulture), // !!!!!!!!!!
+                              ContractStartDate = tf.Footballer.ContractStartDate.ToString("d", CultureInfo.InvariantCulture), //culture-independent format to reach needed format
                               ContractEndDate = tf.Footballer.ContractEndDate.ToString("d", CultureInfo.InvariantCulture),
                               BestSkillType = tf.Footballer.BestSkillType.ToString(),
                               PositionType = tf.Footballer.PositionType.ToString()
@@ -74,6 +86,8 @@
                 .Take(5)
                 .ToArray();
 
+            //Serialize method needs object to convert/map
+	        //adding Formatting for better reading 
             return JsonConvert.SerializeObject(teamsAndFootballers, Formatting.Indented);
         }
     }
